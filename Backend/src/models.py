f07cbbeba5037a80b19466698498dff859eb4cf6
Hiro Nakano <84900813+Rayoo-Nakano@users.noreply.github.com>
2024-06-03 20:11:11 +0900

    Create models.py

---
+++ Backend/src/models.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, Integer, String, Date, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    order_id = Column(String, primary_key=True, index=True)
+    item_code = Column(String, index=True)
+    quantity = Column(Integer)
+
+    allocation_results = relationship("AllocationResult", back_populates="order")
+
+class Inventory(Base):
+    __tablename__ = "inventories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    item_code = Column(String, index=True)
+    quantity = Column(Integer)
+    receipt_date = Column(Date)
+    unit_price = Column(Float)
+
+class AllocationResult(Base):
+    __tablename__ = "allocation_results"
+
+    id = Column(Integer, primary_key=True, index=True)
+    order_id = Column(String, ForeignKey("orders.order_id"))
+    item_code = Column(String, index=True)
+    allocated_quantity = Column(Integer)
+    allocated_price = Column(Float)
+    allocation_date = Column(Date)
+
+    order = relationship("Order", back_populates="allocation_results")
