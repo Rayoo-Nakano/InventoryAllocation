7107f5c59a46979ac6926c68e2b070b1f7c99dd3
Nakano <nakano@rayoo.co.jp>
2024-06-07 22:06:37 +0900

    060768

--- Backend/src/database.py
+++ Backend/src/database.py
@@ -2,32 +2,29 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-from dotenv import load_dotenv
 
-# TODO: シークレットマネージャへの移行が完了したら、.env.testファイルの読み込みを削除してください
-# .env.testファイルのパスを設定する
-env_file_path = os.path.join(os.path.dirname(__file__), ".env.test")
+#ENVIRONMENT = os.environ.get("ENVIRONMENT", "production")
+ENVIRONMENT = "local"
 
-# .env.testファイルが存在するかチェックする
-if not os.path.exists(env_file_path):
-    raise FileNotFoundError(f"Environment file {env_file_path} not found.")
+if ENVIRONMENT == "local":
+    # ローカル環境用の設定
+    print("Running in local environment")
+    SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+else:
+    # 本番環境用の設定
+    print("Running in production environment")
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_PORT = os.environ.get("DB_PORT")
+    DB_NAME = os.environ.get("DB_NAME")
+    DB_USER = os.environ.get("DB_USER")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
 
-# .env.testファイルを読み込む
-load_dotenv(env_file_path)
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
-
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# TODO: 本番環境では、TestingSessionLocalを削除してください
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
