f45a8f67f232e145385d1784bc9935a0b4d19e10
Nakano <nakano@rayoo.co.jp>
2024-06-07 12:33:07 +0900

    060711

--- Backend/test/UT_src/test_main.py
+++ Backend/test/UT_src/test_main.py
@@ -2,7 +2,7 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from main import app, get_db
+from ../../../src/main import app, get_db
 from models import Base
 from schemas import OrderRequest, InventoryRequest, AllocationRequest
 from utils import COGNITO_JWKS_URL, COGNITO_AUDIENCE, COGNITO_ISSUER
