bf46aa9bc388dd44f63c75a1aac0004edaf03595
Nakano <nakano@rayoo.co.jp>
2024-06-07 21:57:40 +0900

    060767

--- Backend/src/database.py
+++ Backend/src/database.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
+# TODO: シークレットマネージャへの移行が完了したら、.env.testファイルの読み込みを削除してください
 # .env.testファイルのパスを設定する
 env_file_path = os.path.join(os.path.dirname(__file__), ".env.test")
 
@@ -13,14 +14,6 @@ if not os.path.exists(env_file_path):
 
 # .env.testファイルを読み込む
 load_dotenv(env_file_path)
-
-# 環境変数の値を出力する
-print("Environment variables loaded from .env.test:")
-print(f"DB_HOST: {os.environ.get('DB_HOST')}")
-print(f"DB_PORT: {os.environ.get('DB_PORT')}")
-print(f"DB_NAME: {os.environ.get('DB_NAME')}")
-print(f"DB_USER: {os.environ.get('DB_USER')}")
-print(f"DB_PASSWORD: {os.environ.get('DB_PASSWORD')}")
 
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
@@ -32,6 +25,9 @@ SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_P
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# TODO: 本番環境では、TestingSessionLocalを削除してください
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
